6d79ead2bbb8b2409d809a86aef53ef72dbec01f
jshabun <jshab003@ucr.edu>
2022-03-09 12:31:54 -0800

    started coding; in-code instructions added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string LongestCommonSubsequence(string x, string y);
+
 int main() {
-    
+    string x = "";
+    string y = "";
+
+    cout << "Longest Common Subsequence" << endl;
+    cout << "--------------------------" << endl;
+    cout << "Enter 2 strings below:" << endl;
+    cout << "X: ";
+    cin >> x;
+    cout << endl;
+    cout << "Y: ";
+    cin >> y;
+
+    LongestCommonSubsequence(x, y);
+
+
+}
+
+string LongestCommonSubsequence(string x, string y) {
+    int i, j = 0;
+
+    i = x.length();
+    j = y.length();
 }
